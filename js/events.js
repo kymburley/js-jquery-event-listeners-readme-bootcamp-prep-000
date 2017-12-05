96fc3eb174f58c88aca6574e6c3fe71d8b949dd9
Kimberly Mozzetti <kimberly.mozzetti@gmail.com>
2017-12-05 16:48:59 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -11,9 +11,18 @@ function frameIt() {
   });
 }
 
+function pressIt() {
+  $("#typing").keydown(function(e) {
+    if (e.which === 71) {
+      alert("You pressed the 'g' key!");
+    }
+  });
+}
+
 $(document).ready(function(){
 
 // call functions here
   getIt();
   frameIt();
+  pressIt();
 });
